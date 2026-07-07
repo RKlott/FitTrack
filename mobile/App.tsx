1391a2c05a7ad52f8a36d610e506fcae403d60bd
Thorsoll <william.cesto14@gmail.com>
2026-07-07 16:18:04 +0200

    initialisation de la version app mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.coucou}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#AAA',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  coucou: {
+    color: "green",
+  }
+});
---
+++ mobile/src/config.ts
@@ -0,0 +1,4 @@
+export const API_URL = "http://192.168.1.93:5000/api" //adresse ip wifi maison
+
+
+//TODO: export const API_URL = "http://xxx.xxx.x.xx:5000/api" //adresse ip wifi AFPA
