613ffd31411f2ea4c3d50db245d4f2953055c98e
Thorsoll <william.cesto14@gmail.com>
2026-07-16 15:21:31 +0200

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './context/AuthContext.tsx';
 import PrivateRoute from './components/PrivateRoute';
-import Layout from './components/Layout/Layout';
+import Layout from './components/Layout/Layout.tsx';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
